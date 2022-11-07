acc34956bb8147ff8d4eb205331f85a14d538e53
Arpit Surana <arpit@permissionles.org>
2022-11-07 18:53:02 +0530

    landing done with mobile resp

--- xumens/src/Components/Home/Home.jsx
+++ xumens/src/Components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css";
 import xumns from "../../Images/xumns.svg";
+import twitterIcon from "../../Images/twitterIcon.svg"
 const Home = () => {
   return (
     <>
@@ -12,7 +13,7 @@ const Home = () => {
           <p>Worlds first Web 3.0 media francise</p>
         </div>
         <div className="button-box">
-            <button></button>
+            <button><img src={twitterIcon} alt="twitter" /> &nbsp;Join Us</button>
         </div>
       </section>
     </>
